c1dd7b546e4b3a83da17f58240644888a8bbfecf
MarioS531 <mario.suman531@gmail.com>
2023-04-15 16:34:13 -0500

    Update php input

--- input.php
+++ input.php
@@ -1,14 +1,14 @@
 <?php
 //Get the form data
-if (isset($_POST['input'])) {
-    $input = $_POST['input'];
-
-    //Save the form data to a file
-    $file = fopen('/input.txt', 'a');
-    fwrite($file, $input);
-    fclose($file);
+if (isset($_POST['songname'])) {
+    $input = $_POST['songname'];
+    echo "$input";
+    // //Save the form data to a file
+    // $file = fopen('input.txt', 'r+') or die("can't open file");
+    // fwrite($file, $input . "\n");
+    // fclose($file);
 
     //Redirect user back to the form
-    //header('Location: index.html');
+//header('Location: index.html');
 }
 ?>
