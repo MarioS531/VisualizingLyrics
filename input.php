4ee2bb3613e7f0b66e38b3864b6d01277c24ac25
MarioS531 <mario.suman531@gmail.com>
2023-04-15 12:23:57 -0500

    Update input.php

--- input.php
+++ input.php
@@ -1,12 +1,14 @@
 <?php
 //Get the form data
-$input = $_POST['input'];
+if (isset($_POST['input'])) {
+    $input = $_POST['input'];
 
-//Save the form data to a file
-$file = fopen('input.txt', 'a');
-fwrite($file, "$input");
-fclose($file);
+    //Save the form data to a file
+    $file = fopen('input.txt', 'a');
+    fwrite($file, $input);
+    fclose($file);
 
-//Redirect user back to the form
-header('Location: index.html');
+    //Redirect user back to the form
+//header('Location: index.html');
+}
 ?>
