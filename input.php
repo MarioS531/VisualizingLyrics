9de4764ae56f4129d78f16fe8668d880cb556dc7
MarioS531 <mario.suman531@gmail.com>
2023-04-15 13:51:13 -0500

    changed the layout (not final)

--- input.php
+++ input.php
@@ -4,11 +4,11 @@ if (isset($_POST['input'])) {
     $input = $_POST['input'];
 
     //Save the form data to a file
-    $file = fopen('input.txt', 'a');
+    $file = fopen('/input.txt', 'a');
     fwrite($file, $input);
     fclose($file);
 
     //Redirect user back to the form
-//header('Location: index.html');
+    //header('Location: index.html');
 }
 ?>
